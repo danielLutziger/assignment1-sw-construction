826aab1b5cf3d4c92270a934fa8f22a6c353db2d
lutzidan <lutzidan@students.zhaw.ch>
2022-11-04 09:38:43 +0100

    complete game.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,4 @@ public class Main {
     public static void main(String[] args) {
         GameOperator.run();
     }
-
-
-    /*
-     *
-     *
-     * TODO: print the game flow in the expected way (outputs from appendix A)
-     *
-     */
-
 }
